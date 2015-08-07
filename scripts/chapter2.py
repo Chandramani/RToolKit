58c0c524f93ea99a7110996807396521e745007c
gs-ctiwary <ctiwary@gainsight.com>
2015-08-07 16:14:31 +0530

    latest changes

--- scripts/chapter2.py
+++ scripts/chapter2.py
@@ -110,7 +110,10 @@ class Chapter2:
     @staticmethod
     def categorical_variable_count(input_col):
         data = Counter(input_col)
-        return data
+        week_count_frame = pd.DataFrame(columns=['count'])
+        for val in data:
+            week_count_frame.loc[val] = data[val]
+        return week_count_frame
 
     @staticmethod
     def derive_mean(input_col):
@@ -190,9 +193,10 @@ class Chapter2:
         :rtype : DataFrame
         :return: Data Frame with column name as index
         """
+        no_of_accounts = input_data_frame['account_id'].nunique()
         summary_frame = pd.DataFrame(columns=(
             'sparsity_flag', 'sparse_percentage', 'per_distinct', 'count_of_distinct', 'min', 'first_quartile', 'mean',
-            'median', 'third_quartile', 'max', 'standard_deviation', 'variance'))
+            'median', 'third_quartile', 'max', 'standard_deviation', 'variance', 'per_zero_accounts_sum'))
         for col in input_data_frame.columns.values:
             if col not in columns_to_ignore:
                 sparsity_flag, sparse_per = self.check_col_sparsity(input_data_col=input_data_frame[col],
@@ -208,8 +212,10 @@ class Chapter2:
                 max_val = input_data_frame[col].max()
                 std = input_data_frame[col].std()
                 variance = input_data_frame[col].var()
+                per_zero_accounts_sum = self.get_per_of_acct_zero_sum(input_data_frame=input_data_frame[['account_id', col]], input_col=col, account_col='account_id', total_no_of_accounts=no_of_accounts)
                 summary_frame.loc[col] = [sparsity_flag, sparse_per, per_distinct, count_of_distinct, min_val,
-                                          first_quartile, mean, median, third_quartile, max_val, std, variance]
+                                          first_quartile, mean, median, third_quartile, max_val, std, variance,
+                                          per_zero_accounts_sum]
         return summary_frame
 
     @staticmethod
@@ -225,12 +231,26 @@ class Chapter2:
     def plot_hist(input_data_frame, bins=10):
         input_data_frame.hist(bins=bins)
 
+    def get_accounts_by_sum(self, input_data_frame=pd.DataFrame(), input_col="", account_col=""):
+        col_sum = self.group_data_frame(df=input_data_frame, metrics_to_group=[input_col], metrics_to_group_by=[account_col], operation="sum")
+        return col_sum
 
+    def get_accounts_by_frequency(self, input_data_frame=pd.DataFrame(), input_col="", account_col=""):
+        col_frequency = self.group_data_frame(df=input_data_frame, metrics_to_group=[input_col], metrics_to_group_by=[account_col], operation="count")
+        return col_frequency
+
+    def get_per_of_acct_zero_sum(self, input_data_frame=pd.DataFrame(), input_col="", account_col="", total_no_of_accounts=int):
+        col_sum = self.get_accounts_by_sum(input_data_frame, input_col, account_col)
+        col_sum = col_sum[col_sum[input_col] == 0]
+        return (len(col_sum)/float(total_no_of_accounts))*100
+
+# also add feature usage coverage
 if __name__ == '__main__':
     obj = Chapter2(mode="prod")
     input_data = pd.read_csv("../data/UsageData.csv")
     input_data['week'] = pd.to_datetime(input_data['week'])
     summary_report = obj.create_summary_report(input_data_frame=input_data, columns_to_ignore=input_data.columns[0:2])
+    summary_report.to_csv("../data_exploration/summary_report.csv")
     account_week_count_frame = obj.get_count_of_week_by_account(input_data_frame=input_data, account_col=['account_id'],
                                                                 week_col=['week'])
     accounts_shortlisted_weeks = Chapter2.filter_accounts_by_col_value(input_data_frame=account_week_count_frame,
@@ -242,29 +262,10 @@ if __name__ == '__main__':
     account_month_count_frame = Chapter2.get_months_for_account(input_data_frame=input_data, account_col="account_id",
                                                                 week_col="week")
     accounts_shortlisted_months = Chapter2.filter_accounts_by_col_value(input_data_frame=account_month_count_frame,
-                                                                      col_name="months", col_value=5, operator="gte")
+                                                                        col_name="months", col_value=5, operator="gte")
     Chapter2.plot_hist(account_week_count_frame)
     Chapter2.plot_hist(account_days_count_frame)
     Chapter2.plot_hist(account_month_count_frame, bins=5)
+    week_count = Chapter2.categorical_variable_count(input_data['week'])
+    week_count.plot()
     plt.show()
-
-    # # sum domain specific exploration
-    # print input_data['week'].min(), input_data['week'].max(), input_data['week'].nunique()
-    # print input_data['account_id'].nunique()
-    # week_account_id_count = input_data[['week', 'account_id']].groupby('week').count().reset_index()
-    # account_week_count = input_data[['week', 'account_id']].groupby('account_id').count().reset_index()
-    # print account_week_count['week'].describe()
-    # print week_account_id_count['account_id'].describe()
-    # print account_week_count.head()
-    # print week_account_id_count.head()
-    # week_account_id_count['week'] = pd.to_datetime(week_account_id_count['week'])
-    # week_account_id_count.set_index('week', inplace=True)
-    # print week_account_id_count.dtypes
-    # print week_account_id_count.head()
-    # fig = plt.figure()
-    # ax1 = fig.add_subplot(2, 1, 1)
-    # ax2 = fig.add_subplot(2, 1, 2)
-    # ax1.hist(account_week_count['week'], bins=20, range=(0, 400))
-    # ax2.hist(account_week_count['week'], bins=20, range=(0, 200))
-    # week_account_id_count.plot()
-    # plt.show()
