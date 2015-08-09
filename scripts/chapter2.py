1a04949f1deb0650dfd9aa33230de2a7346e9211
gs-ctiwary <ctiwary@gainsight.com>
2015-08-09 15:04:28 +0530

    latest changes

--- scripts/chapter2.py
+++ scripts/chapter2.py
@@ -117,6 +117,14 @@ class Chapter2:
         return week_count_frame
 
     @staticmethod
+    def bivariate_categorical_to_categorical(input_col):
+        data = Counter(input_col)
+        week_count_frame = pd.DataFrame(columns=['count'])
+        for val in data:
+            week_count_frame.loc[val] = data[val]
+        return week_count_frame
+
+    @staticmethod
     def derive_mean(input_col):
         input_col.mean()
 
@@ -140,6 +148,46 @@ class Chapter2:
         account_week_count = self.group_data_frame(metrics_to_group=week_col, metrics_to_group_by=account_col,
                                                    df=input_data_frame, operation="count")
         return account_week_count
+
+    def create_summary_report(self, input_data_frame, columns_to_ignore=[]):
+        """
+        :rtype : DataFrame
+        :return: Data Frame with column name as index
+        """
+        no_of_accounts = input_data_frame[account_col].nunique()
+        summary_frame = pd.DataFrame(columns=(
+            'sparsity_flag', 'sparse_percentage', 'per_distinct', 'count_of_distinct', 'min', 'first_quartile', 'mean',
+            'median', 'third_quartile', 'max', 'standard_deviation', 'variance', 'per_zero_accounts_sum'))
+        for col in input_data_frame.columns.values:
+            if col not in columns_to_ignore:
+                sparsity_flag, sparse_per = self.check_col_sparsity(input_data_col=input_data_frame[col],
+                                                                    sparsity_threshold=0.5,
+                                                                    col_name=col)
+                per_distinct, count_of_distinct = self.check_col_count_unique(input_data_col=input_data_frame[col],
+                                                                              col_name=col)
+                min_val = input_data_frame[col].min()
+                first_quartile = np.percentile(input_data_frame[col], 25.0)
+                mean = input_data_frame[col].mean()
+                median = input_data_frame[col].median()
+                third_quartile = np.percentile(input_data_frame[col], 75.0)
+                max_val = input_data_frame[col].max()
+                std = input_data_frame[col].std()
+                variance = input_data_frame[col].var()
+                per_zero_accounts_sum = self.get_per_of_acct_zero_sum(input_data_frame=input_data_frame[[account_col, col]], input_col=col, account_col=account_col, total_no_of_accounts=no_of_accounts)
+                summary_frame.loc[col] = [sparsity_flag, sparse_per, per_distinct, count_of_distinct, min_val,
+                                          first_quartile, mean, median, third_quartile, max_val, std, variance,
+                                          per_zero_accounts_sum]
+        return summary_frame
+
+    @staticmethod
+    def example_cat_var_count_per():
+        s = pd.Series(["a", "b", "c", "a", "a", "b", "c", "a", "a", "b", "c", "a"], dtype="category")
+        cat_var_count = Chapter2.categorical_variable_count(s)
+        print cat_var_count
+        length = len(s)
+        for key in cat_var_count.keys():
+            print key, cat_var_count[key], (cat_var_count[key] / float(length)) * 100
+
 
     @staticmethod
     def get_days_for_accounts(input_data_frame, account_col="", week_col=""):
@@ -189,48 +237,14 @@ class Chapter2:
             print "invalid operator"
             sys.exit(1)
 
-    def create_summary_report(self, input_data_frame, columns_to_ignore=[]):
-        """
-        :rtype : DataFrame
-        :return: Data Frame with column name as index
-        """
-        no_of_accounts = input_data_frame[account_col].nunique()
-        summary_frame = pd.DataFrame(columns=(
-            'sparsity_flag', 'sparse_percentage', 'per_distinct', 'count_of_distinct', 'min', 'first_quartile', 'mean',
-            'median', 'third_quartile', 'max', 'standard_deviation', 'variance', 'per_zero_accounts_sum'))
-        for col in input_data_frame.columns.values:
-            if col not in columns_to_ignore:
-                sparsity_flag, sparse_per = self.check_col_sparsity(input_data_col=input_data_frame[col],
-                                                                    sparsity_threshold=0.5,
-                                                                    col_name=col)
-                per_distinct, count_of_distinct = self.check_col_count_unique(input_data_col=input_data_frame[col],
-                                                                              col_name=col)
-                min_val = input_data_frame[col].min()
-                first_quartile = np.percentile(input_data_frame[col], 25.0)
-                mean = input_data_frame[col].mean()
-                median = input_data_frame[col].median()
-                third_quartile = np.percentile(input_data_frame[col], 75.0)
-                max_val = input_data_frame[col].max()
-                std = input_data_frame[col].std()
-                variance = input_data_frame[col].var()
-                per_zero_accounts_sum = self.get_per_of_acct_zero_sum(input_data_frame=input_data_frame[[account_col, col]], input_col=col, account_col=account_col, total_no_of_accounts=no_of_accounts)
-                summary_frame.loc[col] = [sparsity_flag, sparse_per, per_distinct, count_of_distinct, min_val,
-                                          first_quartile, mean, median, third_quartile, max_val, std, variance,
-                                          per_zero_accounts_sum]
-        return summary_frame
-
     @staticmethod
-    def example_cat_var_count_per():
-        s = pd.Series(["a", "b", "c", "a", "a", "b", "c", "a", "a", "b", "c", "a"], dtype="category")
-        cat_var_count = Chapter2.categorical_variable_count(s)
-        print cat_var_count
-        length = len(s)
-        for key in cat_var_count.keys():
-            print key, cat_var_count[key], (cat_var_count[key] / float(length)) * 100
-
-    @staticmethod
-    def plot_hist(input_data_frame, bins=10):
-        input_data_frame.hist(bins=bins)
+    def get_accounts_non_zero_sum(input_data_frame=pd.DataFrame(), account_col=""):
+        col_sum = input_data_frame.groupby(by=account_col).sum()
+        col_sum = col_sum.sum(axis=1)
+        col_sum.fillna(0, inplace=True)
+        col_sum_none_zero = col_sum[col_sum > 0]
+        col_sum_zero = col_sum[col_sum == 0]
+        return col_sum_none_zero.index, col_sum_zero.index
 
     def get_accounts_by_sum(self, input_data_frame=pd.DataFrame(), input_col="", account_col=""):
         col_sum = self.group_data_frame(df=input_data_frame, metrics_to_group=[input_col], metrics_to_group_by=[account_col], operation="sum")
@@ -246,18 +260,12 @@ class Chapter2:
         return (len(col_sum)/float(total_no_of_accounts))*100
 
     @staticmethod
-    def get_accounts_non_zero_sum(input_data_frame=pd.DataFrame(), account_col=""):
-        col_sum = input_data_frame.groupby(by=account_col).sum()
-        col_sum = col_sum.sum(axis=1)
-        col_sum.fillna(0, inplace=True)
-        col_sum_none_zero = col_sum[col_sum > 0]
-        col_sum_zero = col_sum[col_sum == 0]
-        return col_sum_none_zero.index, col_sum_zero.index
+    def plot_hist(input_data_frame, bins=10):
+        input_data_frame.hist(bins=bins)
 
     @staticmethod
     def create_and_save_correlation_covariance_matrix(input_data_frame, correlation_file_path ,covariance_file_path):
         """
-
         :rtype : Null
         """
         correlation_matrix = input_data_frame[3:usage_data.shape[1]].corr(method='pearson', min_periods=1)
@@ -269,20 +277,20 @@ class Chapter2:
     def read_and_prepare_usage_data(path_to_input_file="../data/UsageData.csv"):
         input_data = pd.read_csv(path_to_input_file)
         input_data[week_col] = pd.to_datetime(input_data[week_col])
+        input_data.fillna(0, inplace=True)
         account_non_zero, account_zero = Chapter2.get_accounts_non_zero_sum(input_data_frame=input_data.drop(week_col, axis=1), account_col=account_col)
         input_data = input_data[input_data[account_col].isin(account_non_zero)]
-        input_data.fillna(0, inplace=True)
         return input_data
 
     @staticmethod
     def read_and_prepare_acct_data(path_to_input_file="../data/AccountsDataOriginal.csv"):
-        account_data = pd.read_csv(path_to_input_file)
-        account_data.fillna(0, inplace=True)
-        account_data.loc[account_data.no_of_users <= 0, 'subscription_value'] = 0
-        account_data.loc[account_data.no_of_users < 0, 'no_of_users'] = 0
-        account_data.loc[account_data.no_of_users <= 0, 'status'] = 'churn'
-        account_data.loc[account_data.no_of_users > 0, 'status'] = 'active'
-        return account_data
+        input_data = pd.read_csv(path_to_input_file)
+        input_data.fillna(0, inplace=True)
+        input_data.loc[input_data.no_of_users <= 0, 'subscription_value'] = 0
+        input_data.loc[input_data.no_of_users < 0, 'no_of_users'] = 0
+        input_data.loc[input_data.no_of_users <= 0, 'status'] = 'churn'
+        input_data.loc[input_data.no_of_users > 0, 'status'] = 'active'
+        return input_data
 
 # also add feature usage coverage
 if __name__ == '__main__':
@@ -290,36 +298,50 @@ if __name__ == '__main__':
     week_col = 'week'
     obj = Chapter2(mode="prod")
     usage_data = Chapter2.read_and_prepare_usage_data("../data/UsageData.csv")
-    # summary_report = obj.create_summary_report(input_data_frame=usage_data, columns_to_ignore=usage_data.columns[0:2])
-    # summary_report.to_csv("../data_exploration/summary_report.csv")
-    # account_week_count_frame = obj.get_count_of_week_by_account(input_data_frame=usage_data, account_col=[account_col],
-    #                                                             week_col=[week_col])
-    # accounts_shortlisted_weeks = Chapter2.filter_accounts_by_col_value(input_data_frame=account_week_count_frame,
-    #                                                                    col_name="week", col_value=39, operator="gte")
-    # account_days_count_frame = Chapter2.get_days_for_accounts(input_data_frame=usage_data, account_col="account_id",
-    #                                                           week_col="week")
-    # accounts_shortlisted_days = Chapter2.filter_accounts_by_col_value(input_data_frame=account_days_count_frame,
-    #                                                                   col_name="days", col_value=39 * 7, operator="gte")
-    # account_month_count_frame = Chapter2.get_months_for_account(input_data_frame=usage_data, account_col="account_id",
-    #                                                             week_col="week")
-    # accounts_shortlisted_months = Chapter2.filter_accounts_by_col_value(input_data_frame=account_month_count_frame,
-    #                                                                     col_name="months", col_value=5, operator="gte")
-    # Chapter2.plot_hist(account_week_count_frame)
-    # Chapter2.plot_hist(account_days_count_frame)
-    # Chapter2.plot_hist(account_month_count_frame, bins=5)
-    # week_count = Chapter2.categorical_variable_count(usage_data[week_col])
-    # week_count.plot()
-    # plt.show()
-    # Chapter2.create_and_save_correlation_covariance_matrix(usage_data[3:usage_data.shape[1]],
-    #                                                        correlation_file_path="../data_exploration/correlation_matrix.csv",
-    #                                                        covariance_file_path="../data_exploration/covariance_matrix.csv")
-    # usage_data.to_csv("../data_exploration/input_data.csv", index=False)
-    # sys.exit(0)
-    # scatter_matrix(usage_data[3:5], alpha=0.2, figsize=(6, 6), diagonal='kde')
-    # plt.show()
-    # b92jt115
-
+    summary_report = obj.create_summary_report(input_data_frame=usage_data, columns_to_ignore=usage_data.columns[0:2])
+    summary_report.to_csv("../data_exploration/summary_report.csv")
+    account_week_count_frame = obj.get_count_of_week_by_account(input_data_frame=usage_data, account_col=[account_col],
+                                                                week_col=[week_col])
+    accounts_shortlisted_weeks = Chapter2.filter_accounts_by_col_value(input_data_frame=account_week_count_frame,
+                                                                       col_name="week", col_value=39, operator="gte")
+    account_days_count_frame = Chapter2.get_days_for_accounts(input_data_frame=usage_data, account_col="account_id",
+                                                              week_col="week")
+    accounts_shortlisted_days = Chapter2.filter_accounts_by_col_value(input_data_frame=account_days_count_frame,
+                                                                      col_name="days", col_value=39 * 7, operator="gte")
+    account_month_count_frame = Chapter2.get_months_for_account(input_data_frame=usage_data, account_col="account_id",
+                                                                week_col="week")
+    accounts_shortlisted_months = Chapter2.filter_accounts_by_col_value(input_data_frame=account_month_count_frame,
+                                                                        col_name="months", col_value=5, operator="gte")
+    Chapter2.plot_hist(account_week_count_frame)
+    Chapter2.plot_hist(account_days_count_frame)
+    Chapter2.plot_hist(account_month_count_frame, bins=5)
+    week_count = Chapter2.categorical_variable_count(usage_data[week_col])
+    week_count.plot()
+    Chapter2.create_and_save_correlation_covariance_matrix(usage_data[3:usage_data.shape[1]],
+                                                           correlation_file_path="../data_exploration/correlation_matrix.csv",
+                                                           covariance_file_path="../data_exploration/covariance_matrix.csv")
     account_data = Chapter2.read_and_prepare_acct_data(path_to_input_file="../data/AccountsDataOriginal.csv")
-    account_non_zero, account_zero = Chapter2.get_accounts_non_zero_sum(input_data_frame=usage_data.drop(week_col, axis=1), account_col=account_col)
-    print account_data.head()
-    # print len(set.intersection(set(usage_data[account_col]), set(account_data[account_col])))
+    # print Chapter2.categorical_variable_count(account_data['status'][account_data[account_col].isin(list(set.intersection(set(usage_data[account_col]), set(account_data[account_col]))))])
+    # for col in usage_data.columns.values:
+    #     if col not in usage_data.columns[0:2]:
+    #         print col
+    #         agg_data = usage_data[[account_col, col]].groupby(account_col).sum().reset_index()
+    #         print Chapter2.categorical_variable_count(account_data['status'][account_data[account_col].isin(agg_data[account_col][agg_data[col] <= 0])])
+
+    usage_data.drop('total_softs_deleted_user_sessions', axis=1, inplace=True)
+    # np.set_printoptions(precision=15)
+    # scatter_matrix(usage_data[14:16], alpha=0.2, figsize=(6, 6), diagonal='kde')
+    # plt.show()
+    # print Chapter2.categorical_variable_count(input_col=account_data['industry'])
+    cross_tab_industry_status = pd.crosstab(account_data['status'], account_data['industry']).apply(lambda x: (x/float(x.sum()))*100, axis=0)
+    usage_data_agg = obj.group_data_frame(df=usage_data,metrics_to_group="all_numeric", metrics_to_group_by=[account_col], operation="sum")
+    usage_data_agg_status = pd.merge(left=usage_data_agg, right=account_data, on=account_col)
+    color_groups = usage_data_agg_status.groupby('status')
+    # Plot
+    fig, ax = plt.subplots()
+    ax.margins(0.05) # Optional, just adds 5% padding to the autoscaling
+    for name, group in color_groups:
+        ax.plot(group.logins, group.transactions, marker='o', linestyle='', ms=12, label=name)
+    ax.legend()
+    # usage_data_agg_status.plot(kind='scatter', x='logins', y='transactions', c='status', s=50)
+    plt.show()
